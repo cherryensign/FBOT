c11792219b18f59e080984038e126a5811f3417e
cherryensign <mithileshghadge44@gmail.com>
2023-04-14 12:26:35 +0530

    UI

--- lib/chatscreen.dart
+++ lib/chatscreen.dart
@@ -24,7 +24,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   void initState() {
     chatGPT = OpenAI.instance.build(
-      token: "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",
+      token: "",
       baseOption: HttpSetup(receiveTimeout: 60000),
     );
     super.initState();
